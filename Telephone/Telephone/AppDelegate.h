9b073ad1b55ed65da869e125862a41528a386e03
monstarlab6 <monstarlab6@monstarlab6deMacBook-Pro.local>
2015-08-28 12:56:02 +0800

    init telphone

---
+++ Telephone/Telephone/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Telephone
+//
+//  Created by monstarlab6 on 15-8-28.
+//  Copyright (c) 2015年 monstarlab6. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Telephone/Telephone/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Telephone
+//
+//  Created by monstarlab6 on 15-8-28.
+//  Copyright (c) 2015年 monstarlab6. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
